d484f58d0379a243eada62eff0cd6b046885f430
Visant <pedrohjaques99@gmail.com>
2025-05-02 15:53:14 -0300

    Update pagarme.ts

--- src/lib/pagarme.ts
+++ src/lib/pagarme.ts
@@ -8,11 +8,14 @@ if (!PAGARME_API_KEY) {
 
 console.log('Configurando cliente Pagar.me com a chave:', PAGARME_API_KEY.substring(0, 10) + '...')
 
+let pagarmeClient
+
 try {
-  const pagarmeClient = client.connect({ api_key: PAGARME_API_KEY })
+  pagarmeClient = client.connect({ api_key: PAGARME_API_KEY })
   console.log('Cliente Pagar.me configurado com sucesso')
-  export const pagarme = pagarmeClient
 } catch (error) {
   console.error('Erro ao configurar cliente Pagar.me:', error)
   throw new Error('Falha ao configurar cliente Pagar.me')
-} 
+}
+
+export const pagarme = pagarmeClient 
